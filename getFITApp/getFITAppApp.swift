777f421df5e72c6a248341e231a1530e4df8656f
clgoerk <Christopherlee.Goerk@triosstudent.com>
2025-01-31 08:48:51 -0500

    Initial Commit

---
+++ getFITApp/getFITAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  getFITAppApp.swift
+//  getFITApp
+//
+//  Created by Chris Goerk on 2025-01-31.
+//
+
+import SwiftUI
+
+@main
+struct getFITAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
